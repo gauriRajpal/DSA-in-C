f4184e4639af7bbfa9df8ae185fc0cee0f213abc
Gauri Rajpal <gaurirajpal2005@gmail.com>
2024-09-28 17:20:05 +0530

    Postfix evaluation using stack

--- Stack/postfix_evaluation.c
+++ Stack/postfix_evaluation.c
@@ -1,69 +1,65 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<ctype.h>
-#define Size 30
+# define s 10
 
 typedef struct Stack{
-    int a[30];
+    int a[s];
     int top;
 }stack;
 
 void push(stack*,int);
 int pop(stack*);
-int postfixeval(char[Size]);
+int postfixeval(char[s]);
 int calc(int,int,char);
 
 int main(){
-    int x,y;
-    char exp[Size];
-    printf("Enter valid postfix expression:\n");
-    scanf("%s",exp);
-    x=postfixeval(exp);
-    printf("The result is:%d\n",x);
+    char express[s];
+    int x, ch;
+    printf("Enter a valid postfix expression-\n");
+    scanf("%s",express);
+    ch = postfixeval(express);
+    printf("Result-\n%d",ch);
 }
+
 void push(stack* st,int x){
-    if(st->top==Size-1){
-        printf("The stack is full\n");
-        return;
-    }
     st->top++;
-    st->a[st->top] = x;   
+    st->a[st->top]=x;
 }
+
 int pop(stack*st){
     int x;
-    if(st->top==-1)
-        printf("The stack is empty");
-    x=st->a[st->top];
+    x = st->a[st->top];
     st->top--;
     return x;
 }
-int postfixeval(char exp[Size]){
+
+int postfixeval(char exp[s]){
     stack st;
-    st.top = -1;
+    st.top=-1;
     int i,lopnd,ropnd,res;
     i=0;
     while(exp[i]!='\0'){
         if(isdigit(exp[i]))
             push(&st,exp[i]-48);
-        else if(exp[i]=='*'||exp[i]=='+'||exp[i]=='-'||exp[i]=='/'){
-            ropnd=pop(&st);
+        else if(exp[i]=='*'||exp[i]=='/'||exp[i]=='+'||exp[i]=='-'){
+            ropnd= pop(&st);
             lopnd=pop(&st);
-            res=calc(lopnd,ropnd,exp[i]);
+            res = calc(lopnd,ropnd,exp[i]);
             push(&st,res);
         }
         else    
-            printf("INAVLID CHARACTER\n");
+            printf("INVALID CHARACTER!!");
         i++;
     }
     return pop(&st);
 }
 
-int calc(int l,int r,char op){
+int calc(int l,int r, char op){
     switch(op){
-        case '+' :return l+r;
-        case '-' : return l-r;
-        case '*' : return l*r;
-        case '/' : return l/r;
+        case '+':   return l+r;
+        case '-':   return l-r;
+        case '*':   return l*r;
+        case '/':   return l/r;
     }
-    return -999;
 }
