c72d41917988dbc3fdf1531d809e35b01d52c317
Gauri Rajpal <gaurirajpal2005@gmail.com>
2024-11-05 14:18:30 +0530

    Insertion at the ending of the list

---
+++ Linked List/Singly/insert_end.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Node{
+    int x;
+    struct Node* next;
+}node;
+
+node*createnode(int);
+node*insert(node*);
+
+int main(){
+    node*start;
+    start=NULL;
+    start=insert(start);
+}
+
+node*createnode(int data){
+    node*nn;
+    nn=(node*)malloc(sizeof(node)); 
+    if(nn!=NULL){
+        nn->x=data;
+        nn->next=NULL;
+    }
+    return nn;
+}
+
+node*insert(node*start){
+    node*nn,*p;
+    int x;
+    printf("Enter the value you want to enter\n");
+    scanf("%d",&x);
+    nn=createnode(x);
+    if(start==NULL){
+        start=nn;
+        nn->next=NULL;
+    }
+    p=start;
+    while(p->next!=NULL)
+        p=p->next;
+    p->next=nn;
+    return start;
+}
